7c27ce3f334d383a5edd4c4fbda31b9c051e503c
marfenox <marfenox@gmail.com>
2024-11-18 17:32:01 -0500

    modificacion config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 module.exports = {
     DB:
 		"mongodb+srv://" +
@@ -16,13 +16,14 @@ module.exports = {
 	SMTP_PASS: process.env.SMTP_PASS,
 
     development: {
-        username:  process.env.USERNAME,
-        password:process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.HOST,
-        dialect: 'mysql',
+        username:process.env.MYSQL_USER || '',
+        password:process.env.MYSQL_PASSWORD || '',
+        database: process.env.MYSQL_DATABASE || '',
+        host: process.env.MYSQL_HOST || '',
+        dialect: 'mysql' || '',
         port: process.env.DB_PORT || 3306
     },
+    
     test: {
         username: process.env.DB_USER || '',
         password: process.env.DB_PASSWORD || '',
@@ -43,3 +44,4 @@ module.exports = {
 
 }
 
+console.log(process.env.USERNAME, process.env.PASSWORD, process.env.DATABASE, process.env.HOST);
